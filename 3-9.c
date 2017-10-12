9fe4b3681a2f0bda2cc5b39f33b423a87b692f18
Kirill Kamakin <kirmanak@gmail.com>
2017-10-12 23:49:20 +0300

    Optimized and works with many spaces

--- 3-9.c
+++ 3-9.c
@@ -6,7 +6,7 @@ char** string_splitter(const char* const string);
 
 int main(int argc, char* argv[])
 {
-    char* string = "one two three four five six";
+    char* string = "      one      two       three       four    five    six    ";
     printf("A string is %s\n", string); 
     char** answer = string_splitter(string);
     for (int i = 0; 6 > i; ++i)
@@ -22,23 +22,24 @@ char** string_splitter(const char* const string)
     char** answer = malloc(sizeof(char*));
     int counter = 0, letters = 0, offset = 0;
     int i = 0;
-    while (1)
+    for (;;)
     {
-        if (string[i] == ' ' || (string[i] == 0 && letters > 0) ) 
+        if (string[i] != ' ') letters++;
+        else offset++; // redunant space in the string
+        if ((string[i] == ' ' || string[i] == 0) && letters > 0) 
         {
             char* word = calloc(letters+1, sizeof(char));
-            strncpy(word, string+offset, letters);
-            word[letters]=0;
-            offset+=1+letters*sizeof(char);
+            strncpy(word, string+offset-1, letters);
+            offset+=letters*sizeof(char);
             counter++;
             letters=0;
             char** tmp = calloc(counter, sizeof(char*));
-            for (int j = 0, limit = counter-1; limit > j; ++j)
-                tmp[j] = answer[j];
-            tmp[counter-1] = word;
+            int last = counter-1;
+            for (int j = 0; last > j; ++j) tmp[j] = answer[j];
+            tmp[last] = word;
             free(answer);
             answer = tmp;
-        } else letters++;
+        }
         if (string[i++] == 0) break;
     }
     return answer;
--- 4-9.c
+++ 4-9.c
@@ -6,12 +6,12 @@ char** string_splitter(const char* const string);
 
 int main(int argc, char* argv[])
 {
-    char* string = "one two three four five six";
+    char* string = "      one      two       three       four    five    six    ";
     printf("A string is %s\n", string); 
     char** answer = string_splitter(string);
     for (int i = 0; 6 > i; ++i)
     {
-        printf("%d-th word is %s \n", i, answer[i]);
+        printf("%d-th word is %s \n", i+1, answer[i]);
         free(answer[i]);
     }
     free(answer);
@@ -19,43 +19,28 @@ int main(int argc, char* argv[])
 
 char** string_splitter(const char* const string)
 {
-    const int size = strlen(string);
     char** answer = malloc(sizeof(char*));
-    int counter = 0;
-    int letters = 0;
-    int offset = 0;
-    for (int i = 0; size > i; ++i)
+    int counter = 0, letters = 0, offset = 0;
+    int i = 0;
+    for (;;)
     {
-        if (string[i] == ' ') 
+        if (string[i] != ' ') letters++;
+        else offset++; // redunant space in the string
+        if ((string[i] == ' ' || string[i] == 0) && letters > 0) 
         {
             char* word = calloc(letters+1, sizeof(char));
-            strncpy(word, string+offset, letters);
-            word[letters]=0;
-            offset+=1+letters*sizeof(char);
+            strncpy(word, string+offset-1, letters);
+            offset+=letters*sizeof(char);
             counter++;
             letters=0;
             char** tmp = calloc(counter, sizeof(char*));
-            for (int j = 0, limit = counter-1; limit > j; ++j)
-                tmp[j] = answer[j];
-            tmp[counter-1] = word;
+            int last = counter-1;
+            for (int j = 0; last > j; ++j) tmp[j] = answer[j];
+            tmp[last] = word;
             free(answer);
             answer = tmp;
-        } else letters++;
-
-    }
-    if (letters > 0)
-    {
-        char* word = calloc(letters, sizeof(char));
-        strncpy(word, string+offset, letters);
-        word[letters]=0;
-        offset+=1+letters*sizeof(char);
-        counter++;
-        char** tmp = calloc(counter, sizeof(char*));
-        for (int j = 0, limit = counter-1; limit > j; ++j)
-            tmp[j] = answer[j];
-        tmp[counter-1] = word;
-        free(answer);
-        answer = tmp;
+        }
+        if (string[i++] == 0) break;
     }
     return answer;
 }
