a1bcb9503927d1651f7452178e9253b4a46e93c2
Kirill Kamakin <kirmanak@gmail.com>
2017-10-12 20:46:53 +0300

    Andrey knows better solution

--- 1-9.c
+++ 1-9.c
@@ -14,10 +14,6 @@ int main(const int argc, char* const argv[])
 
 void fibonacci(int* const arr, const int size)
 {
-    if (size < 2) return;
-    arr[0] = 1;
-    arr[1] = 1;
-    for(int i = 2; size > i; i++)
-        arr[i] = arr[i-1] + arr[i-2];
-    return;
+    for(int i = 0; size > i; i++)
+        arr[i] = (i < 2) ? 1 : arr[i-1]+arr[i-2];
 }
