1264f7650fc1e29d8abbe9955a3815c35addf8a9
Kirill Kamakin <kirmanak@gmail.com>
2017-10-13 19:20:35 +0300

    Caller-saved register is better than callee-saved

--- 3-10.asm
+++ 3-10.asm
@@ -4,18 +4,16 @@ global shake
 ; второй в rsi
 ; целевой буфер в rdx
 shake:
-    push r15        ; счётчик символов в буфере
     xor rcx, rcx    ; счётчик символов в строках
-    xor r15, r15        
+    xor r8, r8      ; счётчик символов в буфере
     .loop:
         mov byte al, [rsi+rcx]
-        mov byte [rdx+r15], al
-        inc r15
+        mov byte [rdx+r8], al
+        inc r8
         mov byte al, [rdi+rcx]
-        mov byte [rdx+r15], al
+        mov byte [rdx+r8], al
         inc rcx
-        inc r15
+        inc r8
         test al, al
         jnz .loop
-    pop r15
     ret
