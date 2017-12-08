895aab8af2e33b3bd1b19da417ac9ac16c96713a
Kirill Kamakin <kirmanak@gmail.com>
2017-12-08 22:39:28 +0300

    Bugfix

--- count.asm
+++ count.asm
@@ -1,21 +1,30 @@
 ; Напишите функцию на ассемблере, которая примет строку и подсчитает количество пропусков в
 ; ней, состоящих из пробелов или табуляции (вперемешку).
+%macro get_symbol 0
+  mov byte cl, [rdi + rdx]
+  inc rdx
+%endmacro
+
 section .text
 global count
-; указатель в rsi
+; указатель в rdi
 count:
     xor ecx, ecx                ; текущий символ 
     xor eax, eax                ; cчётчик пропусков
     xor edx, edx                ; счётчик символов
     .loop:
-        mov byte cl, [rsi+rdx]  ; берём следующий символ
+        get_symbol
         jrcxz .end
-        inc rdx
+        cmp cl, ' '             ; space?
+        je .skip
         cmp cl, 0x09            ; tab?
-        je .inc
-        cmp cl, 0x20            ; space?
         jne .loop
-    .inc:
+    .skip:
+        get_symbol
+        cmp cl, 0x09
+        je .skip                ; большая куча табов
+        cmp cl, ' '
+        je .skip                ; большая куча пробелов
         inc rax
         jmp .loop
     .end:
